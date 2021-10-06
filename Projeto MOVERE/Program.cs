4671576b0669e21db056f1f506ef31750791bf12
Izabella Queiroz Franco Barbosa <91679090+BellaFranco@users.noreply.github.com>
2021-10-06 19:57:28 -0300

    Acerto de código

--- Projeto MOVERE/Program.cs
+++ Projeto MOVERE/Program.cs
@@ -18,79 +18,68 @@ namespace Projeto_MOVERE
             cat = new Categoria(); // construtor da classe
             Contas contas;
             contas = new Contas(); // construtor da classe
+            // Definições de variáveis para utilização posterior
             int opcao;
+            string nomeBanco, nomeCategoria, descricao, nomeBancoConta, categoriaConta;
+            float saldoBanco, valor;
         // AQUI É APENAS O MENU INICAL PARA VISUALIZACAO
         MenuInicial:
             Console.Clear();
-            Console.WriteLine("==============MENU==================");
-            Console.WriteLine("1 - Cadastrar Banco e Saldo");
-            Console.WriteLine("2 - Cadastrar Categoria");
-            Console.WriteLine("3 - Listar Bancos e Saldo");
-            Console.WriteLine("4 - Listar Categorias");
-            Console.WriteLine("5 - Lançar Contas");
-            Console.WriteLine("6 - Listar Saldo Bancario");
-            Console.WriteLine("7 - Sair");
-            Console.WriteLine("====================================");
+            Console.WriteLine("==============MENU==================\n1 - Cadastrar Banco e Saldo\n2 - Cadastrar Categoria\n3 - Listar Bancos e Saldo\n4 - Listar Categorias\n5 - Lançar Contas\n6 - Listar Contas e Saldos\n7 - Sair\n====================================");
             Console.Write("Sua opção: ");
             // VARIAVEL QUE RECEBE A OPCAO DIGITADA
             opcao = int.Parse(Console.ReadLine());
             switch(opcao)
             {
                 case 1:
-                    // uso de variaveis para receber os dados de nome e saldo
-                    string nomeBanco;
-                    float saldoBanco;
                     Console.Write("Digite o nome do banco: ");
                     nomeBanco = Console.ReadLine();
                     Console.Write("Digite o saldo incial: ");
                     saldoBanco = float.Parse(Console.ReadLine());
-                    // funcao da classe pra criar o banco
+                    // metodo da classe pra criar o banco
                     cadbancario.CriarBanco(nomeBanco, saldoBanco);
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
                 case 2:
-                    string nomeCategoria;
                     Console.Write("Digite a categoria: ");
                     nomeCategoria = Console.ReadLine();
+                    // metodo pra criar categoria
                     cat.CadastroCategoria(nomeCategoria);
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
                 case 3:
+                    // metodo de exibicao de saldos
                     cadbancario.ExibirSaldos();
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
                 case 4:
+                    // metodo pra exibir categorias
                     cat.ExibirCategorias();
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
                 case 5:
-                    string descricao;
-                    float valor;
-                    string nomeBancoConta;
-                    string categoriaConta;
-                    Console.Write("Descricao: ");
+                    Console.Write("Descrição: ");
                     descricao = Console.ReadLine();
                     Console.Write("Valor da conta: ");
                     valor = float.Parse(Console.ReadLine());
                     cadbancario.ListarBancos();
                     Console.Write("Escolha o banco: ");
                     opcao = int.Parse(Console.ReadLine());
-                    nomeBancoConta = cadbancario.SelecionarBanco(opcao);
+                    nomeBancoConta = cadbancario.SelecionarBanco(opcao); // retorna nome do banco de acordo com a posicao
                     cadbancario.AtualizarSaldo(opcao, valor);
                     cat.ExibirCategorias();
                     Console.Write("Escolha a categoria: ");
                     opcao = int.Parse(Console.ReadLine());
-                    categoriaConta = cat.SelecionarCategoria(opcao);
+                    categoriaConta = cat.SelecionarCategoria(opcao); // retorna nome da categoria de acordo com a posicao
                     contas.LancarConta(descricao, valor, categoriaConta, nomeBancoConta);
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
                 case 6:
-                    cadbancario.ExibirSaldos();
                     Console.Write("Pressione ENTER para voltar ao menu");
                     Console.ReadLine();
                     goto MenuInicial;
